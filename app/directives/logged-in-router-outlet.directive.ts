1c6f5507393b2453957704b9c55c53dc833b3bd7
Brian Black <bblack@veracode.com>
2016-04-15 12:24:01 -0700

    adding .gitignore and updating tabs logged-in-router-outlet

--- app/directives/logged-in-router-outlet.directive.ts
+++ app/directives/logged-in-router-outlet.directive.ts
@@ -3,40 +3,38 @@ import { Router, RouterOutlet, ComponentInstruction } from 'angular2/router';
 import {IAuthService} from '../services/iauth.service';
 
 @Directive({
-    selector: 'router-outlet'
+  selector: 'router-outlet'
 })
 
 export class LoggedInRouterOutlet extends RouterOutlet {
-    private router: Router;
+  private router: Router;
 
-    constructor(
-        _elementRef: ElementRef, _loader: DynamicComponentLoader,
-        _parentRouter: Router, @Attribute('name') nameAttr: string,
-        private _authService: IAuthService
-    ) {
-        super(_elementRef, _loader, _parentRouter, nameAttr);
+  constructor(
+    _elementRef: ElementRef, _loader: DynamicComponentLoader,
+    _parentRouter: Router, @Attribute('name') nameAttr: string,
+    private _authService: IAuthService
+  ) {
+    super(_elementRef, _loader, _parentRouter, nameAttr);
+    this.router = _parentRouter;
+  }
 
-        this.router = _parentRouter;
+  activate(instruction: ComponentInstruction) {
+    var roles = <string[]> instruction.routeData.data['roles'];
+
+    // no defined roles means it is a public route	
+    if (roles == null) {
+      return super.activate(instruction);
     }
 
-    activate(instruction: ComponentInstruction) {
-        var roles = <string[]> instruction.routeData.data['roles'];
+    // if the user isn't autheticated
+    if (this._authService.isLoggedIn()) {
+      if (roles.length == 0 || this._authService.hasRole(roles)) {
+        return super.activate(instruction);
+      } else {
+        this.router.navigate(['Dash']); //navigate to default route
+      }
+    }	    
 
-        // no defined roles means it is a public route	
-	if (roles == null) {
-	    return super.activate(instruction);
-	}
-
-	// if the user isn't autheticated
-	if (this._authService.isLoggedIn()) {
-	    if (roles.length == 0 || this._authService.hasRole(roles)) {
-                return super.activate(instruction);
-	    } else {
-                this.router.navigate(['Dash']); //navigate to default route
-            }
-        }	    
-
-        this.router.navigate(['Login']);
-    }
-
+    this.router.navigate(['Login']);
+  }
 }
