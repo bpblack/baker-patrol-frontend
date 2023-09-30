d2af3905b8dc71d0541c81fcfe8eda2af8ca7314
Brian Black <bblack@veracode.com>
2023-09-29 18:45:06 -0700

    Adding play.google.com domain.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     //apiUrl: 'https://volypatrol.mtbaker.us/api'
     apiUrl: 'http://localhost:3000/api',
-    allowedDomains: ['localhost:3000']
+    allowedDomains: ['localhost:3000', 'play.google.com']
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://mtbakervolyapi-06e43d72d3ac.herokuapp.com/api',
-    allowedDomains: ['mtbakervolyapi-06e43d72d3ac.herokuapp.com']
+    allowedDomains: ['mtbakervolyapi-06e43d72d3ac.herokuapp.com', 'play.google.com']
 };
