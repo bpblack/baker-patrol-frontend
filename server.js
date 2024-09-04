6f9d77713610e67d8c098d37bcf4ce1c09d74cde
Brian Black <bblack@veracode.com>
2024-09-03 20:32:49 -0700

    Heroku push try 2

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ let enforce = require('express-sslify')
 let app = express();
 app.use(enforce.HTTPS({ trustProtoHeader: true }), express.static(__dirname+'/dist/baker-patrol-frontend'));
 app.get('/*', (req, resp) => {
-  resp.sendFile(__dirname+'/dist/baker-patrol-frontend/index.html')
+  resp.sendFile('./dist/baker-patrol-frontend/index.html')
 });
 app.listen(process.env.PORT || 8080);
